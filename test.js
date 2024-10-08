276702c60922c296844752dca5036f51251bead4
Voiculescu Bogdan <voiculescubogdan22@stud.ase.ro>
2024-10-08 16:19:58 +0300

    variabile

---
+++ test.js
@@ -0,0 +1,13 @@
+function test() {
+    if(true) {
+        var var1 = 1;
+        const const1 = 2;
+        let let3 = 3;
+    }
+
+    console.log(var1);
+    console.log(const1);
+    console.log(let3);
+}
+
+console.log(var1);
