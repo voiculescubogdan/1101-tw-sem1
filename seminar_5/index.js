a97dbc6eab3c24051978a58769e0039cd235f5e2
bogdanvoiculescu <voiculescubogdan22@stud.ase.ro>
2024-11-05 16:22:12 +0200

    seminar 5 bonus

--- seminar_5/index.js
+++ seminar_5/index.js
@@ -7,6 +7,7 @@ const express = require('express');
 const users = require('./users');
 const cors = require('cors');
 const app = express();
+const number = 0;
 
 app.use(cors({
     origin: 'http://127.0.0.1:5500'
@@ -20,6 +21,26 @@ app.get('/users', function(req, res) {
     const dbUsers = users;
 
     res.status(200).send(dbUsers);
+})
+
+app.get('/users/:id', function(req, res) {
+    const userId = req.params.id ? Number(req.params.id) : null;
+    
+
+    console.log(userId);
+
+    if(!userId) {
+        throw new Error('User id not found');
+        
+    }
+
+    const user = users.find(user => user.id === userId);
+
+    if(user) {
+        res.status(200).send(user);
+    } else {
+        throw new Error(`User with id ${userId} not found`);
+    }
 })
 
 app.listen(3000, () => {
